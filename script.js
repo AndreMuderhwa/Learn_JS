805401456deff0b3dbab78a9acb5fed521430391
AndreMuderhwa <safariandre66@gmail.com>
2024-01-31 11:45:49 +0100

    basics of js

---
+++ script.js
@@ -0,0 +1,24 @@
+// console.log("Hello World")
+// name="Andrew Muderhwa"
+// age=24
+//comment
+
+//console.log(name)
+//console.log(age)
+
+
+food =Number(prompt("How much was your food ??"))
+//variables
+tipPercentage= Number(prompt("Tip %"))/100 
+//operator
+tipAmount= food * tipPercentage
+total=food+tipAmount
+
+console.log('Tip Amount',tipAmount)
+console.log('Total',total)
+
+//user input
+// 23 MIN 
+
+
+
