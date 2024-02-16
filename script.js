764aa43d4f2902c7ff52a01f5d3c0e89c449e279
AndreMuderhwa <safariandre66@gmail.com>
2024-02-16 10:15:18 +0100

    functions

--- script.js
+++ script.js
@@ -39,5 +39,41 @@ if rain grab your embrella else wear your sunglass
 //     console.log('Wear your sunglasses')
 // }
 
+//FUNCTIONS
 
+function SayMyNAME(name){
+    console.log(name)
+}
 
+SayMyNAME('Andrew')
+
+function greetings(name){
+    greet=`Hi ${name} Nice to meet you !!`
+    console.log(greet)
+}
+
+greetings('Anna')
+
+function sum(a ,b){
+    return a+b
+}
+
+console.log(sum(12,4))
+
+//BABY TIP CALCULATOR
+//old declaration of function
+function CalculateFoodTotal(food, tip){
+    const tipPercentage= tip/100 
+   
+    const tipAmount= food * tipPercentage
+    const total=sum(food,tipAmount)
+    return total
+}
+
+console.log(CalculateFoodTotal(100,20))
+
+//ARROW FUNCTIONS
+
+const SumArrow = (a, b) =>{
+    return a + b
+}
