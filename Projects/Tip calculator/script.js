066fc52ee5749d57df257ddf5f96a89c8fa8b2ec
AndreMuderhwa <safariandre66@gmail.com>
2024-03-05 07:34:43 +0100

    tip calculator

--- Projects/Tip calculator/script.js
+++ Projects/Tip calculator/script.js
@@ -1,21 +1,34 @@
+const billTotalInput = document.getElementById('billTotalInput')
+const tipInput = document.getElementById('tipInput')
+const numberPeople = document.getElementById('numberOfPeople')
+const personTotalDiv = document.getElementById('perPersonTotal')
+let numberOfPeople = Number(numberPeople.innerText)
 
-const billInput=document.getElementById('billTotalInput')
-const  perPersonTotalDiv=document.getElementById('perPersonTotal')
-const  tipInput =document.getElementById('tipInput')
-const  numberPeople=document.getElementById('numberOfPeople')
 
-let numberOfPeople=Number(numberPeople.innerText)
 
 const calculateBill =() =>{
- const bill =Number(billInput.value)
- const tipPercentage=Number(tipInput.value) / 100
- const tipAmount = bill * tipPercentage
- const total =tipAmount + bill
- const personTotal =total / numberOfPeople
- perPersonTotalDiv.innerText=`$${personTotal}`
-
+    const bill = billTotalInput.value
+    const tipPercentage = Number(tipInput.value) / 100
+    const tipAmount = bill * tipPercentage
+    const total = tipAmount + bill
+    const perPerson = total / numberOfPeople
+    personTotalDiv.innerText=`$${perPerson.toFixed(2).toLocaleString('en-US')}`
 }
 
-const increasePeople =()=>{}
+const increasePeople = () =>{
+    numberOfPeople += 1
+    numberPeople.innerText=numberOfPeople
+    calculateBill()
+}
 
-const decreasePeople =() =>{}
+
+const decreasePeople =() =>{
+    if(numberOfPeople <= 1){
+        return
+    }
+    numberOfPeople -= 1
+    numberPeople.innerText=numberOfPeople
+    calculateBill()
+    
+
+}
