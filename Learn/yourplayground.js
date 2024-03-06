44a08bf3582aec6b18a43242c605a856033c27c1
AndreMuderhwa <safariandre66@gmail.com>
2024-03-06 08:01:54 +0100

    second exercice

--- Learn/yourplayground.js
+++ Learn/yourplayground.js
@@ -8,4 +8,21 @@ const RandomFruit = (fruit ) =>{
  
  const fruits =['banana','tomato','orange','avocado','apple']
  console.log(RandomFruit(fruits))
- 
+
+ // exercice about rain, sunny overcast
+ // rainny ==1 sunny ==-1 overcast == 0
+
+
+ const weatherScore = (weather) =>{
+    let score 
+    if (weather =='rainy'){
+        score=1
+    }else if(weather == 'sunny'){
+        score = -1
+    }else{
+        score = 0
+    }
+
+ }
+
+ //5:18:19
