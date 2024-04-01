93afca390e9a283aa5c7ac32b2f2ad63332a7000
AndreMuderhwa <safariandre66@gmail.com>
2024-04-01 11:34:25 +0200

    add some features

--- Advanced JS/Projects/FightingGame/script.js
+++ Advanced JS/Projects/FightingGame/script.js
@@ -8,6 +8,17 @@ let p1HealthDiv = document.getElementById('p1Health')
 let p2HealthDiv = document.getElementById('p2Health')
 
 const updateGame =(p1,p2,gameState) =>{
+    p1NameDiv.innerText=p1.name
+    p2NameDiv.innerText=p2.name
+    p1HealthDiv.innerText=p1.health
+    p2HealthDiv.innerText=p2.health
+
+    if(p1.health <=0 || p2.health<=0){
+        game.isOver=true
+        gameState=game.isOver
+        resultDiv.innerText=game.declareWinner(game.isOver,p1,p2)
+        return gameState
+    }
 
 }
 
@@ -20,11 +31,12 @@ class Player{
         this.attackDamage=attackDamage
     }
     strike(player,enemy, attackDamage){
+        
 
     }
 
     heal(player){
-          
+
     }
 }
 
@@ -43,14 +55,19 @@ class Game{
 
 
         while(!this.isOver){
-            
+
         }
     }
 }
 
+let player1 = new Player('Andrew',120,10)
+let player2 = new Player('Aldridge',130,10)
 
-let p1;
-let p2;
+let p1 =player1;
+let p2 =player2;
+
+let game = new Game()
+updateGame(p1,p2,game.isOver)
 
 let gameState;
 
@@ -60,4 +77,5 @@ document.addEventListener('keydown', function(e){
 
 document.addEventListener('keydown', function(e){
 
-});v
+});
+
